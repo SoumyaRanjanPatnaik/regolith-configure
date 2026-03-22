4711242d892c24095484eafcd3ccad87f8888dda
Soumya Ranjan Patnaik <soumyaranjan1812@gmail.com>
2026-03-22 20:19:35 +0530

    fix: enable tests define in the tests.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,12 @@ pub mod commands;
 pub mod config;
 pub mod resources;
 pub mod search;
-#[cfg(test)]
-pub mod test_utils;
-
 pub use cli_args::get_session_type;
 pub use commands::{SearchResult, search_config, set_user_xresource};
 pub use config::{ConfigPartial, FullConfig, SessionMappings};
+
+// Testing related modules only compiled when running tests
+#[cfg(test)]
+pub mod test_utils;
+#[cfg(test)]
+pub mod tests;
